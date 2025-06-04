15034d635a8a59d9b4c9df7ce340ee6520520e36
Shubham <111904549+shubhamsapkal2912@users.noreply.github.com>
2025-06-04 22:15:23 +0530

    Add files via upload

---
+++ tms/tweet/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from . models import Tweet
+# Register your models here.
+#added
+admin.site.register(Tweet)
---
+++ tms/tweet/forms.py
@@ -0,0 +1,19 @@
+#added
+
+from django import forms
+from . models import Tweet
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class TweetForm(forms.ModelForm):
+    class Meta:
+        model=Tweet
+        fields = ['text','photo']  #these are from models.py
+        
+
+class UserRegistrationForm(UserCreationForm): 
+    email=forms.EmailField()
+    
+    class Meta:
+        model=User
+        fields=('username','email','password1','password2')      
